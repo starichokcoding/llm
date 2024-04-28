27f35d59390ccf15b5eeeaa3899a4b52f7bd0f87
starichokcoding <166571862+starichokcoding@users.noreply.github.com>
2024-04-28 15:13:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,7 +188,7 @@ df = pd.read_csv('loan_dr_aws_apr2024_numeric.csv')
 # df_new.to_csv('loan_dr_aws_smpl_apr2024.csv', index = False)
 X, y = df.drop('is_bad', axis=1), df[['is_bad']]
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
-d_train = xgboost.DMatrix(X_df, label=y_train)
+d_train = xgboost.DMatrix(X, label=y_train)
 # d_test = xgboost.DMatrix(X_test, label=y_test)
 
 params = {
