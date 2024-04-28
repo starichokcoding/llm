683096ff52cc3c8836168e2a7c93e585403fba71
starichokcoding <166571862+starichokcoding@users.noreply.github.com>
2024-04-28 17:08:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,28 +183,48 @@ import shap
 import pandas as pd
 
 df = pd.read_csv('loan_dr_aws_apr2024_numeric.csv')
-
 X, y = df.drop('is_bad', axis=1), df[['is_bad']]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
-d_train = xgboost.DMatrix(X_train, label=y_train)
-d_test = xgboost.DMatrix(X_test, label=y_test)
+
+def load_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+    d_train = xgboost.DMatrix(X_train, label=y_train)
+    d_test = xgboost.DMatrix(X_test, label=y_test)
+    params = {
+        "eta": 0.01,
+        "objective": "binary:logistic",
+        "subsample": 0.5,
+        "base_score": np.mean(y_train),
+        "eval_metric": "logloss",
+        "n_jobs": -1,
+    }
+    model = xgboost.train(params, d_train, 10, evals = [(d_test, "test")], verbose_eval=100, early_stopping_rounds=20)
+    return model
+
+model = load_model(X, y)
 
 
-params = {
-    "eta": 0.01,
-    "objective": "binary:logistic",
-    "subsample": 0.5,
-    "base_score": 0.5,
-    "eval_metric": "logloss",
-}
-model = xgboost.train(
-    params,
-    d_train,
-    5000,
-    evals=[(d_test, "test")],
-    verbose_eval=100,
-    early_stopping_rounds=20,
-)
+
+# X, y = df.drop('is_bad', axis=1), df[['is_bad']]
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+# d_train = xgboost.DMatrix(X_train, label=y_train)
+# d_test = xgboost.DMatrix(X_test, label=y_test)
+
+
+# params = {
+#     "eta": 0.01,
+#     "objective": "binary:logistic",
+#     "subsample": 0.5,
+#     "base_score": 0.5,
+#     "eval_metric": "logloss",
+# }
+# model = xgboost.train(
+#     params,
+#     d_train,
+#     5000,
+#     evals=[(d_test, "test")],
+#     verbose_eval=100,
+#     early_stopping_rounds=20,
+# )
 
 # explainer = shap.Explainer(model, X)
 # shap_values = explainer(X)
